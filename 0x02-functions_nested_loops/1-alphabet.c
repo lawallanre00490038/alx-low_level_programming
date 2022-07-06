e116e56b1b1129f8d9a1c9726030beb31ebb4841
Lawal Olanrewaju <lawallanre@larriestringues.localdomain>
2022-07-06 13:13:02 +0100

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *main - prints putchar
+ * main - Entry point
  *
- *Description: 'The programme description'
+ * Description: 'the program's description'
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
