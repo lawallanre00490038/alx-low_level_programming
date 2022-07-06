0a5d5abed0f724fb6876add0a1e68b74d3a82c73
Lawal Olanrewaju <lawallanre@larriestringues.localdomain>
2022-07-06 13:57:07 +0100

    test

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -6,15 +6,6 @@
 
 void print_alphabet_x10(void)
 {
-int j;
-int x;
-
-for (x = 0; x < 10; x++)
-{
-for (j = 'a'; j <= 'z'; j++)
-{
-_putchar(j);
-}
-}
-_putchar('\n');
+print_alphabet_x10();
+return (0);
 }
