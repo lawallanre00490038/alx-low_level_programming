2b6e9c28bd0164f593f80ba691e01b744ad0c060
Lawal Olanrewaju <lawallanre.abbahouse.gamil.com>
2022-07-06 02:14:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
 #include <main.h>
 
-/**main - prints putchar
- * Return: always 0
+/**
+ * main - prints putchar
+ *
+ *Description: 'The programme description'
+ *
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-_putchar("_");
-_putcahr("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putcahr('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
