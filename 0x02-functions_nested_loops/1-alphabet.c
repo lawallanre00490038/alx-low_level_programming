fdd9af8fc5cd1db6a238352f20b5fe24f832ddfb
laawallanre00490038 <lawallanre.abbahouse@gmail.com>
2022-07-06 12:38:18 +0100

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,10 @@
 
 void print_alphabet(void)
 {
-    int j;
-    for (j = 'a'; j <= 'z'; j++)
-    {
-        _putchar(j);
-    }
-    _putchar('\n');
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
 }
