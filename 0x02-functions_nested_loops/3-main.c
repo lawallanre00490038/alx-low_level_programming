63a5e74eda6c66cd1ad09099e08d9ec48b9cab0c
Lawal Olanrewaju <lawallanre.abbahouse.gamil.com>
2022-07-07 04:20:48 -0700

    test

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int r;
-r =test_islower('H');
+r = test_islower('H');
 _putchar(r + '0');
-r = _islower('o');
+r = test_islower('o');
 _putchar(r + '0');
-r = _islower(108);
+r = test_islower(108);
 _putchar(r + '0');
 _putchar('\n');
 return (0);
