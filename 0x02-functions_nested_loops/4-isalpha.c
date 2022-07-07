24052a523a24f727aa72ac4b1bc546743e54e760
Lawal Olanrewaju <lawallanre.abbahouse.gamil.com>
2022-07-07 04:41:57 -0700

    test

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_islower - Checks if a character is lowercase
+ *_isalpha - Checks if a character is lowercase
  *@c: the character to be checked
  *
  *Return: 1 if character is lowercase, 0 otherwise
