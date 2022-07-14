7de17734e8cecc515541b1f40b5fbdf0faa52e91
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-07-14 09:36:28 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,6 @@ char *strcat(char *dest, const char *src)
 int index = 0, dest_len = 0;
 
 while (dest[index++])
-{
 dest_len++;
 
 for (index = 0; src[index]; index++)
@@ -22,4 +21,3 @@ dest[dest_len++] = src[index];
 return (dest);
 }
 
-}
