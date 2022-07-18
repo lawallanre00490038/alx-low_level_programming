b65b0e0592b21c5c14592ef4611bd9e4a2e0d9cf
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-07-18 08:23:20 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
+ * _memCpy - Copies @n bytes from the memory area pointed
  *           to by @src into that pointed to by @dest.
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
