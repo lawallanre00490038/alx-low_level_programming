c9d795f97a6e8814e0601b98793b94893c5a0f8d
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-07-25 09:23:43 +0100

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/*
+ * _put will put
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
+
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
