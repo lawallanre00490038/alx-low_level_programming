4941b6dee554394263a4db2d9bf47ec3705ce3f8
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-07-27 10:44:26 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
  * Return: (0) for success
  */
 
-int main(int argc, char *argv[] __attribute__((__unused__)))
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-printf("%d\n", argv - 1);
+printf("%d\n", argc - 1);
 return (0);
 }
