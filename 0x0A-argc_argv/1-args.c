d0174d095c77cc0a3fee5dbb5ac0f603a217a7a2
Lawal Olanrewaju <lawallanre.abbahouse.gamil.com>
2022-07-27 02:39:06 -0700

    test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main -> this is the function
+ * @argc: argc parameter
+ * @argv: argv parameter
+ * Return: (0) for success
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[] __attribute__((__unused__)))
+{
+printf("%d\n", argv - 1);
+return (0);
+}
