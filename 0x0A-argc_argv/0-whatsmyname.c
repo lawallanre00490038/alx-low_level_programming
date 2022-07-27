56a4dfec80eedacff09840d44b5e57f9984abec3
Lawal Olanrewaju <lawallanre.abbahouse.gamil.com>
2022-07-27 02:19:19 -0700

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main -> this is the function
+ * @argc: argc parameter
+ * @argv: argv parameter
+ * Return: (0) for success
+ */
+
+int main (int argc, char *argv[])
+{
+printf ("%s\n", *argv);
+}
