e975a20a25025259db9bcab903c315cd112f0cb7
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2022-10-31 07:54:44 -0700

    test

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Prints the size of various types based on
+ *
+ * the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
